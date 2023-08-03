7f02999f19d99c0b7d61fc308e57ed5b0a1af8cc
k.luo <k.luo@herzogdemeuron.com>
2023-08-03 19:06:07 +0200

    try combobox filter

--- Settings/ItemSetting.cs
+++ Settings/ItemSetting.cs
@@ -5,11 +5,13 @@ using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows.Data;
+using System.Linq;
 
 namespace RevitGuide.Settings
 {
-    public class ItemSetting
+    public class ItemSetting : INotifyPropertyChanged
     {
         private string _key;
         public string Key
@@ -49,27 +51,64 @@ namespace RevitGuide.Settings
             get => UriHelper.StringToUri(Uri);
         }
 
-        public ObservableCollection<RvtCommand> AllRvtCommands { get => new ObservableCollection<RvtCommand>(RvtCommandHelper.AllRvtCommands); }
-        private ICollectionView _allRvtCmdCollection = null;
-        public ICollectionView AllRvtCmdCollection
-        {
-            get
-            {
-                if (_allRvtCmdCollection == null && AllRvtCommands != null)
-                {
-                    _allRvtCmdCollection = CollectionViewSource.GetDefaultView(AllRvtCommands);
-                }
 
-                return _allRvtCmdCollection;
+        private ObservableCollection<RvtCommand> _allRvtCommands;
+        public ObservableCollection<RvtCommand> AllRvtCommands
+        {
+            get { return _allRvtCommands; }
+            set
+            {
+                if(value != _allRvtCommands)
+                {
+                    _allRvtCommands = value;
+                    OnPropertyChanged(nameof(AllRvtCommands));
+                }
             }
         }
+
+
+        private ObservableCollection<RvtCommand> _filteredRvtCommands;
+        public ObservableCollection<RvtCommand> FilteredRvtCommands
+        {
+            get { return _filteredRvtCommands; }
+            set
+            {
+                if(value != _filteredRvtCommands)
+                {
+                    _filteredRvtCommands = value;
+                    OnPropertyChanged(nameof(FilteredRvtCommands));
+                }
+            }
+        }
+
+        private string _searchText;
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                if(value != _searchText)
+                {
+                    _searchText = value;
+                    OnPropertyChanged(nameof(SearchText));
+                    FilteredRvtCommands = new ObservableCollection<RvtCommand>(AllRvtCommands.Where(x => x.Name.ToLower().Contains(SearchText.ToLower())));
+                }
+            }
+        }
+
         public ItemSetting(string key = null , string uri = "")
         {
             Key = key;
             Uri = uri;
+            AllRvtCommands = new ObservableCollection<RvtCommand>(RvtCommandHelper.AllRvtCommands);
         }
 
-       
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
     }
 }
--- Views/SearchComboBox.cs
+++ Views/SearchComboBox.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RevitGuide.Views
 {
@@ -19,22 +20,49 @@ namespace RevitGuide.Views
             base.OnApplyTemplate();
 
             editableTextBox = GetTemplateChild("PART_EditableTextBox") as TextBox;
-
-            editableTextBox.TextChanged += EditableTextBox_TextChanged;
+            if (editableTextBox != null)
+            {
+                editableTextBox.TextChanged += EditableTextBox_TextChanged;
+                editableTextBox.PreviewTextInput += EditableTextBox_PreviewTextInput;
+            }
         }
+        private void EditableTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
 
+            e.Handled = true;
+
+            // Check if the entire text is selected
+            if (editableTextBox.SelectionLength == editableTextBox.Text.Length)
+            {
+                // Replace the text
+                editableTextBox.Text = e.Text;
+            }
+            else
+            {
+                // Append the new text to the existing text
+                editableTextBox.Text += e.Text;
+            }
+
+            // Set the caret to the end of the text
+            editableTextBox.CaretIndex = editableTextBox.Text.Length;
+            // deselect all text
+            //IsDropDownOpen = true;
+            editableTextBox.SelectionLength = 0;
+            editableTextBox.SelectionBrush = null;
+        }
         private void EditableTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (ItemsSource is ICollectionView ICV)
             {
                 if (string.IsNullOrEmpty(editableTextBox.Text.Trim()))
+                {
                     ICV.Filter = null;
+                }
                 else
+                {
                     ICV.Filter = new Predicate<object>(i => ((RvtCommand)i).Name.ToLower().Contains(editableTextBox.Text.ToLower()));
-
-                //IsDropDownOpen = true;
+                }
             }
-
         }
     }
 }
